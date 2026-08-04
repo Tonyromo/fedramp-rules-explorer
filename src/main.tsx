d64cfe7e19609a381a9826d1f4cc10ce85fb0db8
Tonyromo <52567052+Tonyromo@users.noreply.github.com>
2026-08-04 15:26:22 -0400

    Load milestone three styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './review.css'
 import './milestone2.css'
+import './milestone3.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
