315ad518b70bcca8f37fcbd4f1dd409c22cb58ce
Tonyromo <52567052+Tonyromo@users.noreply.github.com>
2026-08-04 14:58:59 -0400

    Load milestone two styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './review.css'
+import './milestone2.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
